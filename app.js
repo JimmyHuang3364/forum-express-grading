4efff78de5241045b69a9885b1514670cde18f84
JimmyHuang3364 <open3364@hotmail.com>
2021-11-13 19:54:15 +0800

    update PORT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const db = require('./models') // 引入資料庫
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.use(express.urlencoded({ extended: true }))
