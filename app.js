2dd2b4fae12e174a26eb3c1624ca61d284edb487
JimmyHuang3364 <open3364@hotmail.com>
2021-11-13 19:14:27 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const passport = require('./config/passport')
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+app.use(express.urlencoded({ extended: true }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) // Handlebars 註冊樣板引擎
+app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use('/upload', express.static(__dirname + '/upload'))
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
 })
 
-require('./routes')(app)
+app.use(methodOverride('_method'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app, passport) // 把 passport 傳入 routes
 
 module.exports = app
