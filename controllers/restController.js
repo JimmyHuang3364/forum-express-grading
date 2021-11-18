deee4b023a9b420402f74945060e983d2a902e6f
JimmyHuang3364 <open3364@hotmail.com>
2021-11-19 04:25:55 +0800

    修正按鈕顯示問題

--- controllers/restController.js
+++ controllers/restController.js
@@ -58,12 +58,17 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-
+        { model: User, as: 'FavoritedUsers' }, // 加入關聯資料
+        { model: User, as: 'LikedUsers' },
         { model: Comment, include: [User] }
       ]
     }).then(restaurant => {
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id) // 找出收藏此餐廳的 user
+      const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
       return res.render('restaurant', {
-        restaurant: restaurant.toJSON()
+        restaurant: restaurant.toJSON(),
+        isFavorited: isFavorited,
+        isLiked: isLiked
       })
     })
   },
