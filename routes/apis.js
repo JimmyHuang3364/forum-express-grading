3a8181fff7bb0f2e23b9b1e3b2dabf0dcc50271d
JimmyHuang3364 <open3364@hotmail.com>
2021-11-25 23:26:14 +0800

    add token

--- routes/apis.js
+++ routes/apis.js
@@ -5,6 +5,9 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
+
+router.post('/signin', userController.signIn)
 
 //restaurants
 router.get('/admin/restaurants', adminController.getRestaurants)
@@ -18,7 +21,5 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
-
-
 
 module.exports = router
